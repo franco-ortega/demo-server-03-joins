83dad94daaf7059c07d7bb4ea91d868da5713074
Franco Ortega <francopdx@gmail.com>
2021-08-08 15:56:09 -0700

    add GET route for Tile

--- __tests__/tiles.test.js
+++ __tests__/tiles.test.js
@@ -4,7 +4,7 @@ const request = require('supertest');
 const pool = require('../lib/utils/pool');
 
 describe('Endpoint tests for Tile model', () => {
-    beforeEach(() => pool.query(fs.readFileSync('./sql/database.sql', 'utf-8')));
+    beforeAll(() => pool.query(fs.readFileSync('./sql/database.sql', 'utf-8')));
 
     afterAll(() => pool.end());
 
@@ -33,14 +33,14 @@ describe('Endpoint tests for Tile model', () => {
         const response = await request(app)
             .get('/api/v1/tiles');
 
-            expect(response.body).toEqual(
-                {
-                    id: '1',
-                    material: 'ceramic',
-                    shape: 'square',
-                    color: 'green',
-                    cost: 2
-                }
-            );
+        expect(response.body).toEqual(
+            [{
+                id: '1',
+                material: 'ceramic',
+                shape: 'square',
+                color: 'green',
+                cost: 2
+            }]
+        );
     });
 });
