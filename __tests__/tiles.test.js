1ef723a621c5e667fa6e25f5a8e67acb276d010a
Franco Ortega <francopdx@gmail.com>
2021-08-15 21:59:32 -0700

    update tiles tests

--- __tests__/tiles.test.js
+++ __tests__/tiles.test.js
@@ -3,6 +3,7 @@ const app = require('../lib/app');
 const request = require('supertest');
 const pool = require('../lib/utils/pool');
 const Floor = require('../lib/models/Floor');
+const Tile = require('../lib/models/Tile');
 
 describe('Endpoint tests for Tile model', () => {
     beforeEach(() => pool.query(fs.readFileSync('./sql/database.sql', 'utf-8')));
@@ -10,13 +11,11 @@ describe('Endpoint tests for Tile model', () => {
     afterAll(() => pool.end());
 
     it('POST: creates a new Tile', async() => {
-        const floor = await Floor.insert(
-            {
-                room: 'kitchen',
-                length: 10,
-                width: 5
-            }
-        );
+        const floor = await Floor.insert({
+            room: 'kitchen',
+            length: 10,
+            width: 5
+        });
 
         const response = await request(app)
             .post('/api/v1/tiles')
@@ -28,26 +27,22 @@ describe('Endpoint tests for Tile model', () => {
                 floorId: floor.id
             });
 
-            expect(response.body).toEqual(
-                {
-                    id: '1',
-                    material: 'ceramic',
-                    shape: 'square',
-                    color: 'green',
-                    cost: 2,
-                    floorId: floor.id
-                }
-            );
+        expect(response.body).toEqual({
+            id: '1',
+            material: 'ceramic',
+            shape: 'square',
+            color: 'green',
+            cost: 2,
+            floorId: floor.id
+        });
     });
 
     it('GET: gets all Tiles', async() => {
-        const floor = await Floor.insert(
-            {
-                room: 'kitchen',
-                length: 10,
-                width: 5
-            }
-        );
+        const floor = await Floor.insert({
+            room: 'kitchen',
+            length: 10,
+            width: 5
+        });
 
         await request(app)
             .post('/api/v1/tiles')
@@ -72,38 +67,32 @@ describe('Endpoint tests for Tile model', () => {
         const response = await request(app)
             .get('/api/v1/tiles');
 
-        expect(response.body).toEqual(
-            [{
-                id: '1',
-                material: 'ceramic',
-                shape: 'square',
-                color: 'green',
-                cost: 2,
-                floorId: floor.id
-            },
-            {
-                id: '2',
-                material: 'stone',
-                shape: 'circle',
-                color: 'grey',
-                cost: 5,
-                floorId: floor.id
-            }]
-        );
+        expect(response.body).toEqual([{
+            id: '1',
+            material: 'ceramic',
+            shape: 'square',
+            color: 'green',
+            cost: 2,
+            floorId: floor.id
+        },
+        {
+            id: '2',
+            material: 'stone',
+            shape: 'circle',
+            color: 'grey',
+            cost: 5,
+            floorId: floor.id
+        }]);
     });
 
     it('GET: gets one tile by id', async() => {
-        const floor = await Floor.insert(
-            {
-                room: 'kitchen',
-                length: 10,
-                width: 5
-            }
-        );
+        const floor = await Floor.insert({
+            room: 'kitchen',
+            length: 10,
+            width: 5
+        });
 
-        await request(app)
-            .post('/api/v1/tiles')
-            .send({
+        const tile = await Tile.insert({
                 material: 'ceramic',
                 shape: 'square',
                 color: 'green',
@@ -112,41 +101,35 @@ describe('Endpoint tests for Tile model', () => {
             });
 
         const response = await request(app)
-            .get('/api/v1/tiles/1');
+            .get(`/api/v1/tiles/${tile.id}`);
 
-        expect(response.body).toEqual(
-            {
+        expect(response.body).toEqual({
                 id: '1',
                 material: 'ceramic',
                 shape: 'square',
                 color: 'green',
                 cost: 2,
                 floorId: floor.id
-            }
-        );
+        });
     });
 
     it('PUT: updates a Tile by id', async() => {
-        const floor = await Floor.insert(
-            {
+        const floor = await Floor.insert({
                 room: 'kitchen',
                 length: 10,
                 width: 5
-            }
-        );
+        });
 
-        await request(app)
-            .post('/api/v1/tiles')
-            .send({
-                material: 'ceramic',
-                shape: 'square',
-                color: 'green',
-                cost: 2,
-                floorId: floor.id
-            });
+        const tile = await Tile.insert({
+            material: 'ceramic',
+            shape: 'square',
+            color: 'green',
+            cost: 2,
+            floorId: floor.id
+        });
 
         const response = await request(app)
-            .put('/api/v1/tiles/1')
+            .put(`/api/v1/tiles/${tile.id}`)
             .send({
                 material: 'ceramic',
                 shape: 'square',
@@ -155,49 +138,41 @@ describe('Endpoint tests for Tile model', () => {
                 floorId: floor.id
             });
 
-            expect(response.body).toEqual(
-                {
-                    id: '1',
-                    material: 'ceramic',
-                    shape: 'square',
-                    color: 'green',
-                    cost: 5,
-                    floorId: floor.id
-                }
-            );
+        expect(response.body).toEqual({
+            id: '1',
+            material: 'ceramic',
+            shape: 'square',
+            color: 'green',
+            cost: 5,
+            floorId: floor.id
+        });
     });
 
     it('DELETE: deletes a Tile by id', async() => {
-        const floor = await Floor.insert(
-            {
-                room: 'kitchen',
-                length: 10,
-                width: 5
-            }
-        );
+        const floor = await Floor.insert({
+            room: 'kitchen',
+            length: 10,
+            width: 5
+        });
 
-        await request(app)
-            .post('/api/v1/tiles')
-            .send({
-                material: 'ceramic',
-                shape: 'square',
-                color: 'green',
-                cost: 5,
-                floorId: floor.id
-            });
+        const tile = await Tile.insert({
+            material: 'ceramic',
+            shape: 'square',
+            color: 'green',
+            cost: 5,
+            floorId: floor.id
+        });
 
         const response = await request(app)
-            .delete('/api/v1/tiles/1');
+            .delete(`/api/v1/tiles/${tile.id}`);
 
-            expect(response.body).toEqual(
-                {
-                    id: '1',
-                    material: 'ceramic',
-                    shape: 'square',
-                    color: 'green',
-                    cost: 5,
-                    floorId: floor.id
-                }
-            );
+        expect(response.body).toEqual({
+            id: '1',
+            material: 'ceramic',
+            shape: 'square',
+            color: 'green',
+            cost: 5,
+            floorId: floor.id
+        });
     });
 });
